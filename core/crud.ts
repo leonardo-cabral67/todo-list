32f6dc7bc190c27f47a7447da59a255c7b4d6c05
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-05-12 21:41:29 -0300

    feat: create read operation for crud

---
+++ core/crud.ts
@@ -0,0 +1,22 @@
+import { readFileSync } from "fs";
+
+const DB_FILE_PATH = "./core/db";
+
+type UUID = string;
+
+interface ITodo {
+  id: UUID;
+  content: string;
+  done: boolean;
+}
+
+export function read(): ITodo[] {
+  const dbString = readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+
+  if (!db) {
+    return [];
+  }
+
+  return db.todos;
+}
