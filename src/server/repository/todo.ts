b8a4304191dacf6f1dcbd3a46686abcf89f9c108
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-05-18 16:14:06 -0300

    feat: implement todo server repository get function

---
+++ src/server/repository/todo.ts
@@ -0,0 +1,55 @@
+import { read } from "@db-crud-todo";
+
+type UUID = string;
+
+interface Todo {
+  id: UUID;
+  content: string;
+  date: string;
+  done: boolean;
+}
+
+interface GetTodoRepositoryInput {
+  page?: number;
+  limit?: number;
+}
+
+interface OutputGetTodoRepository {
+  todos: Todo[];
+  total: number;
+  pages: number;
+}
+
+function get({
+  page,
+  limit,
+}: GetTodoRepositoryInput = {}): OutputGetTodoRepository {
+  const currentPage = page || 1;
+  const currentLimit = limit || 2;
+  const ALL_TODOS = read();
+  const totalTodos = ALL_TODOS.length;
+  const totalPages = calculatePages(totalTodos, currentLimit);
+
+  const todosPaginated = paginate(currentPage, currentLimit, ALL_TODOS);
+  return {
+    todos: todosPaginated,
+    total: totalTodos,
+    pages: totalPages,
+  };
+}
+
+function paginate(page: number, limit: number, todos: Todo[]) {
+  const startIndex: number = (page - 1) * limit;
+  const endIndex: number = page * limit;
+  const todosPaginated = todos.slice(startIndex, endIndex);
+  return todosPaginated;
+}
+
+function calculatePages(todosLength: number, limit: number) {
+  const pages: number = Math.ceil(todosLength / limit);
+  return pages;
+}
+
+export const todoRepository = {
+  get,
+};
