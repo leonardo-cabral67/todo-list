86e421f239d59342b11c2df09855e5459078aaf5
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-05-23 01:27:21 -0300

    feat: create function createByContent

--- core/crud.ts
+++ core/crud.ts
@@ -1,4 +1,5 @@
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
+import { v4 as uuid } from "uuid";
 
 const DB_FILE_PATH = "./core/db";
 
@@ -9,6 +10,22 @@ interface ITodo {
   content: string;
   date: string;
   done: boolean;
+}
+
+export function createByContent(content: string): ITodo {
+  const todo: ITodo = {
+    id: uuid(),
+    content: content,
+    date: new Date().toISOString(),
+    done: false,
+  };
+
+  const ALL_TODOS = read();
+  const todos = [...ALL_TODOS, todo];
+
+  writeFileSync(DB_FILE_PATH, JSON.stringify({ todos }, null, 1));
+
+  return todo;
 }
 
 export function read(): ITodo[] {
